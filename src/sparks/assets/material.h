b880710d34be3a04505e2357e80570bd8ea2fe31
Hercier <shiz20@mails.tsinghua.edu.cn>
2023-01-11 21:00:33 +0800

    add assimp temp

--- src/sparks/assets/material.h
+++ src/sparks/assets/material.h
@@ -15,6 +15,10 @@ struct Material {
   glm::vec3 albedo_color{0.8f};
   int albedo_texture_id{0};
   MaterialType material_type{MATERIAL_TYPE_LAMBERTIAN};
-  int reserve[3];
+  int specular_texure_id{0};
+  int normal_texure_id{0};
+  int height_texure_id{0};
+  int texture_id[3];
+ //int reserve[3];
 };
 }  // namespace sparks
